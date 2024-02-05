16b7080322350a76c1d2c63213a917d9d24d1e74
Akshay Dahiya <akshay@powerloom.io>
2024-02-05 19:28:56 +0530

    chore: cleanup, fix python version, improve ipfs config

--- build-dev.sh
+++ build-dev.sh
@@ -84,19 +84,19 @@ if [ "$POWERLOOM_REPORTING_URL" ]; then
     echo "Found SLACK_REPORTING_URL ${POWERLOOM_REPORTING_URL}";
 fi
 
-./build-docker.sh;
+docker build -t powerloom-snapshotter-lite .
 
 echo "building...";
 
 if ! [ -x "$(command -v docker-compose)" ]; then
     echo 'docker compose not found, trying to see if compose exists within docker';
-    if [ "$IPFS_URL" ]; then
+    if [ "$IPFS_URL" == "/dns/ipfs/tcp/5001" ]; then
         docker compose -f docker-compose-dev.yaml --profile ipfs up -V --abort-on-container-exit
     else
         docker compose -f docker-compose-dev.yaml up --no-deps -V --abort-on-container-exit
     fi
 else
-    if [ "$IPFS_URL" ]; then
+    if [ "$IPFS_URL" == "/dns/ipfs/tcp/5001" ]; then
         docker-compose -f docker-compose-dev.yaml --profile ipfs up -V --abort-on-container-exit
     else
         docker-compose -f docker-compose-dev.yaml up --no-deps -V --abort-on-container-exit
