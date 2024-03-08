7cfe7570ffc9a80c955990a202a7474c3d5f0788
Akshay Dahiya <akshay@powerloom.io>
2024-03-08 19:12:57 +0530

    chore: update chain and contract address configs

--- snapshotter_autofill.sh
+++ snapshotter_autofill.sh
@@ -4,9 +4,9 @@ source .env
 
 if [ -z "$OVERRIDE_DEFAULTS" ]; then
     echo "reset to default values...";
-    export PROST_RPC_URL="https://rpc-prost1c.powerloom.io"
-    export PROTOCOL_STATE_CONTRACT="0x803727326Bd602f5E2e490FCA26c4E52dB883B9f"
-    export PROST_CHAIN_ID="103"
+    export PROST_RPC_URL="https://rpc-prost1d.powerloom.io"
+    export PROTOCOL_STATE_CONTRACT="0xa028e377719B1F6399b415D85DA1F6ec77C82823"
+    export PROST_CHAIN_ID="104"
 fi
 
 echo 'populating setting from environment values...';
