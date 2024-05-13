24b9d68738425e0dc0172ef671d144133f7087dc
muku314115 <mukangotan@gmail.com>
2024-05-13 18:18:25 +0800

    undo

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ echo "setting up codebase...";
 rm -rf submission-sequencer;
 git clone https://github.com/PowerLoom/submission-sequencer.git;
 cd ./submission-sequencer;
-git checkout feat/consensus;
+git checkout relayer_secure;
 cd ..;
 
 echo "bootstrapping complete!";
