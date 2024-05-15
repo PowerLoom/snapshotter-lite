020e3374bcfff42d6efa4118405274efa61efa4b
muku314115 <mukangotan@gmail.com>
2024-05-15 13:21:20 +0800

    updating bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ echo "setting up codebase...";
 rm -rf submission-sequencer;
 git clone https://github.com/PowerLoom/submission-sequencer.git;
 cd ./submission-sequencer;
-git checkout relayer_secure;
+git checkout no_dashboard;
 cd ..;
 
 echo "bootstrapping complete!";
