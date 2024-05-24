524475f4da1b92e234dcaa66b2405f673f579f46
muku314115 <mukangotan@gmail.com>
2024-05-24 15:50:49 +0800

    minor corrections

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@ source .env
 
 echo "setting up codebase...";
 
-rm -rf submission-sequencer;
+rm -rf snapshotter-lite-local-collector;
 git clone https://github.com/PowerLoom/snapshotter-lite-local-collector.git;
 cd ./snapshotter-lite-local-collector;
 git checkout simulation_node_bi;
